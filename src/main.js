f50ad515b688028aa3a95c4cc62dda79830cd807
sustained <sustained.dissonance@gmail.com>
2019-11-27 05:11:44 +0000

    Load CSS.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import "@/assets/styles/main.scss";
+
 Vue.config.productionTip = false;
 
 new Vue({
