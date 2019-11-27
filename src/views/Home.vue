b1a7b2f9d01f3089894c25c7c0b5af00a4dd5eeb
sustained <sustained.dissonance@gmail.com>
2019-11-27 05:12:38 +0000

    Display grid.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <grid />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Grid from "@/components/Grid.vue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: { Grid }
 };
 </script>
